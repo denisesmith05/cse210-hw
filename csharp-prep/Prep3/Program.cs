8f549797ccb07b04f53927f9bf185f7975e303a6
Denise <denise.sunshine@icloud.com>
2024-06-21 14:28:49 -0700

    Finished prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,39 @@
 using System;
+using System.Runtime.InteropServices.Marshalling;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        string play_again;
+        do{
+            int guess = 0;
+            int counter = 0;
+            int magic_num = randomGenerator.Next(1, 100);
+
+            while (guess != magic_num){
+                Console.Write("What is your guess? ");
+                guess = int.Parse(Console.ReadLine());
+                counter += 1;
+
+                if (guess > magic_num){
+                    Console.WriteLine("Lower");
+                }
+                else if (guess < magic_num){
+                    Console.WriteLine("Higher");
+                }
+                else{
+                    Console.WriteLine("You guessed it! ");
+                    Console.WriteLine($"You guessed {counter} time(s). ");
+                }
+            }
+            Console.Write("Do you want to play again? ");
+            play_again = Console.ReadLine();
+
+        } while (play_again == "yes");
+        if (play_again == "no"){
+            Console.Write("Thanks for playing! Goodbye!");
+        }
     }
 }
