ea1984f4260e46f7d6cabe0b49c3353e0acf3ec9
Denise <denise.sunshine@icloud.com>
2024-07-01 21:40:53 -0700

    adding learning 02

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,9 +1,67 @@
+// My code:
+
 using System;
+using System.Threading.Tasks.Dataflow;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job {
+            _company = "Microsoft",
+            _jobTitle = "Software Engineer",
+            _startYear = 2019,
+            _endYear = 2022
+        };
+
+        Job job2 = new Job {
+            _company = "Apple",
+            _jobTitle = "Manager",
+            _startYear = 2022,
+            _endYear = 2023
+        };
+
+        Resume myResume = new Resume {
+            _name = "Allison Rose",
+        };
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        Console.WriteLine($"Name: {myResume._name}");
+        Console.WriteLine("Jobs:");
+        foreach (var job in myResume._jobs) {
+            string jobInfo = job.DisplayJobDetails();
+            Console.WriteLine(jobInfo);
+        }
     }
 }
+
+// Teacher's code:
+
+// using System;
+
+// class Program
+// {
+//     static void Main(string[] args)
+//     {
+//         Job job1 = new Job();
+//         job1._jobTitle = "Software Engineer";
+//         job1._company = "Microsoft";
+//         job1._startYear = 2019;
+//         job1._endYear = 2022;
+
+//         Job job2 = new Job();
+//         job2._jobTitle = "Manager";
+//         job2._company = "Apple";
+//         job2._startYear = 2022;
+//         job2._endYear = 2023;
+
+//         Resume myResume = new Resume();
+//         myResume._name = "Allison Rose";
+
+//         myResume._jobs.Add(job1);
+//         myResume._jobs.Add(job2);
+
+//         myResume.Display();
+//     }
+// }
---
+++ prepare/Learning02/Resume.cs
@@ -0,0 +1,32 @@
+// My code:
+
+public class Resume {
+    public string _name;
+    public string _title;
+    public List<Job> _jobs = new List<Job>();
+}
+
+// Teacher's code:
+
+// using System;
+
+// public class Resume
+// {
+//     public string _name;
+
+//     // Make sure to initialize your list to a new List before you use it.
+//     public List<Job> _jobs = new List<Job>();
+
+//     public void Display()
+//     {
+//         Console.WriteLine($"Name: {_name}");
+//         Console.WriteLine("Jobs:");
+
+//         // Notice the use of the custom data type "Job" in this loop
+//         foreach (Job job in _jobs)
+//         {
+//             // This calls the Display method on each job
+//             job.Display();
+//         }
+//     }
+// }
