d48d59f09ac4ae62800b75469f8f40bda0cca6f8
Denise <denise.sunshine@icloud.com>
2024-07-24 14:36:53 -0700

    added foundation 3

--- final/Foundation3/Program.cs
+++ final/Foundation3/Program.cs
@@ -1,3 +1,132 @@
+// using System;
+// using System.Collections.Generic;
+
+// public class Address
+// {
+//     private string _street;
+//     private string _city;
+//     private string _state;
+//     private string _country;
+
+//     public Address(string street, string city, string state, string country)
+//     {
+//         _street = street;
+//         _city = city;
+//         _state = state;
+//         _country = country;
+//     }
+
+//     public override string ToString()
+//     {
+//         return $"{_street}, {_city}, {_state}, {_country}";
+//     }
+// }
+
+// public class Event
+// {
+//     private string _title;
+//     private string _description;
+//     private string _date;
+//     private string _time;
+//     private Address _address;
+
+//     public Event(string title, string description, string date, string time, Address address)
+//     {
+//         _title = title;
+//         _description = description;
+//         _date = date;
+//         _time = time;
+//         _address = address;
+//     }
+
+//     public virtual string StandardDetails()
+//     {
+//         return $"Title: {_title}\nDescription: {_description}\nDate: {_date}\nTime: {_time}\nAddress: {_address}";
+//     }
+
+//     public virtual string FullDetails()
+//     {
+//         return StandardDetails();
+//     }
+
+//     public virtual string ShortDescription()
+//     {
+//         return $"{GetType().Name} Event: {_title} on {_date}";
+//     }
+// }
+
+// public class Lecture : Event
+// {
+//     private string _speaker;
+//     private int _capacity;
+
+//     public Lecture(string title, string description, string date, string time, Address address, string speaker, int capacity)
+//         : base(title, description, date, time, address)
+//     {
+//         _speaker = speaker;
+//         _capacity = capacity;
+//     }
+
+//     public override string FullDetails()
+//     {
+//         return $"{base.FullDetails()}\nSpeaker: {_speaker}\nCapacity: {_capacity}";
+//     }
+// }
+
+// public class Reception : Event
+// {
+//     private string _rsvpEmail;
+
+//     public Reception(string title, string description, string date, string time, Address address, string rsvpEmail)
+//         : base(title, description, date, time, address)
+//     {
+//         _rsvpEmail = rsvpEmail;
+//     }
+
+//     public override string FullDetails()
+//     {
+//         return $"{base.FullDetails()}\nRSVP Email: {_rsvpEmail}";
+//     }
+// }
+
+// public class OutdoorGathering : Event
+// {
+//     private string _weather;
+
+//     public OutdoorGathering(string title, string description, string date, string time, Address address, string weather)
+//         : base(title, description, date, time, address)
+//     {
+//         _weather = weather;
+//     }
+
+//     public override string FullDetails()
+//     {
+//         return $"{base.FullDetails()}\nWeather: {_weather}";
+//     }
+// }
+
+// public class Program
+// {
+//     public static void Main()
+//     {
+//         var address = new Address("789 Park Ave", "New York", "NY", "USA");
+
+//         var lecture = new Lecture("Tech Talk", "A talk on the latest in tech", "01 Aug 2024", "10:00 AM", address, "John Tech", 100);
+//         var reception = new Reception("Networking Event", "An event to network with peers", "02 Aug 2024", "6:00 PM", address, "rsvp@event.com");
+//         var outdoorGathering = new OutdoorGathering("Summer BBQ", "A fun BBQ event", "03 Aug 2024", "12:00 PM", address, "Sunny");
+
+//         var events = new List<Event> { lecture, reception, outdoorGathering };
+
+//         foreach (var evt in events)
+//         {
+//             Console.WriteLine(evt.StandardDetails());
+//             Console.WriteLine(evt.FullDetails());
+//             Console.WriteLine(evt.ShortDescription());
+//             Console.WriteLine();
+//         }
+//     }
+// }
+
 using System;
 using System.Collections.Generic;
 
@@ -22,7 +151,7 @@ public class Address
     }
 }
 
-public class Event
+public abstract class Event
 {
     private string _title;
     private string _description;
@@ -30,7 +159,7 @@ public class Event
     private string _time;
     private Address _address;
 
-    public Event(string title, string description, string date, string time, Address address)
+    protected Event(string title, string description, string date, string time, Address address)
     {
         _title = title;
         _description = description;
@@ -44,10 +173,7 @@ public class Event
         return $"Title: {_title}\nDescription: {_description}\nDate: {_date}\nTime: {_time}\nAddress: {_address}";
     }
 
-    public virtual string FullDetails()
-    {
-        return StandardDetails();
-    }
+    public abstract string FullDetails();
 
     public virtual string ShortDescription()
     {
@@ -69,7 +195,7 @@ public class Lecture : Event
 
     public override string FullDetails()
     {
-        return $"{base.FullDetails()}\nSpeaker: {_speaker}\nCapacity: {_capacity}";
+        return $"{StandardDetails()}\nType: Lecture\nSpeaker: {_speaker}\nCapacity: {_capacity}";
     }
 }
 
@@ -85,7 +211,7 @@ public class Reception : Event
 
     public override string FullDetails()
     {
-        return $"{base.FullDetails()}\nRSVP Email: {_rsvpEmail}";
+        return $"{StandardDetails()}\nType: Reception\nRSVP Email: {_rsvpEmail}";
     }
 }
 
@@ -101,7 +227,7 @@ public class OutdoorGathering : Event
 
     public override string FullDetails()
     {
-        return $"{base.FullDetails()}\nWeather: {_weather}";
+        return $"{StandardDetails()}\nType: Outdoor Gathering\nWeather: {_weather}";
     }
 }
 
