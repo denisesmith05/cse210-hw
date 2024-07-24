3a3a57e3416f7b150ae478b88d6226e37bf3b012
Denise <denise.sunshine@icloud.com>
2024-07-24 11:45:20 -0700

    fixed all the camel casing

--- final/Foundation1/Program.cs
+++ final/Foundation1/Program.cs
@@ -3,94 +3,90 @@ using System.Collections.Generic;
 
 public class Comment
 {
-    public string Name { get; set; }
-    public string Text { get; set; }
+    private string _name;
+    private string _text;
 
     public Comment(string name, string text)
     {
-        Name = name;
-        Text = text;
+        _name = name;
+        _text = text;
     }
 
     public override string ToString()
     {
-        return $"{Name}: {Text}";
+        return $"{_name}: {_text}";
     }
 }
 
 public class Video
 {
-    public string Title { get; set; }
-    public string Author { get; set; }
-    public int Length { get; set; }
-    private List<Comment> Comments { get; set; }
+    private string _title;
+    private string _author;
+    private int _length; // in seconds
+    private List<Comment> _comments;
 
     public Video(string title, string author, int length)
     {
-        Title = title;
-        Author = author;
-        Length = length;
-        Comments = new List<Comment>();
+        _title = title;
+        _author = author;
+        _length = length;
+        _comments = new List<Comment>();
     }
 
     public void AddComment(Comment comment)
     {
-        Comments.Add(comment);
+        _comments.Add(comment);
     }
 
     public int GetNumberOfComments()
     {
-        return Comments.Count;
+        return _comments.Count;
     }
 
     public void Display()
     {
-        Console.WriteLine($"Title: {Title}");
-        Console.WriteLine($"Author: {Author}");
-        Console.WriteLine($"Length: {Length} minutes");
+        Console.WriteLine($"Title: {_title}");
+        Console.WriteLine($"Author: {_author}");
+        Console.WriteLine($"Length: {_length} seconds");
         Console.WriteLine($"Number of Comments: {GetNumberOfComments()}");
         Console.WriteLine("Comments:");
-        foreach (var comment in Comments)
+        foreach (var comment in _comments)
         {
             Console.WriteLine(comment);
         }
-        Console.WriteLine();
     }
 }
 
-class Program
+public class Program
 {
-    static void Main()
+    public static void Main()
     {
-        List<Video> videos = new List<Video>();
-
-        Video video1 = new Video("C# Tutorial", "John Doe", 20);
-        video1.AddComment(new Comment("Alice", "Great tutorial!"));
-        video1.AddComment(new Comment("Bob", "Very helpful."));
+        var video1 = new Video("Introduction to C#", "John Doe", 300);
+        video1.AddComment(new Comment("Alice", "Great video!"));
+        video1.AddComment(new Comment("Bob", "Very helpful, thanks!"));
         video1.AddComment(new Comment("Charlie", "Thanks for the tips."));
-        videos.Add(video1);
-
-        Video video2 = new Video("Cooking Pasta", "Jane Smith", 12);
-        video2.AddComment(new Comment("Dave", "Looks delicious!"));
-        video2.AddComment(new Comment("Eve", "Can't wait to try this."));
+        
+        var video2 = new Video("Advanced C# Techniques", "Jane Smith", 450);
+        video2.AddComment(new Comment("Charlie", "Loved the advanced topics."));
+        video2.AddComment(new Comment("Dave", "Learned a lot!"));
         video2.AddComment(new Comment("Frank", "Yummy!"));
-        videos.Add(video2);
 
-        Video video3 = new Video("How to Make Cloud Slime", "Sandy Lee", 7);
+        var video3 = new Video("How to Make Cloud Slime", "Sandy Lee", 7);
         video3.AddComment(new Comment("Wanda", "Such a fun activity for the kids!"));
         video3.AddComment(new Comment("Kara", "So pretty!"));
         video3.AddComment(new Comment("Harry", "Very soft and relaxing."));
-        videos.Add(video3);
 
-        Video video4 = new Video("Is the earth really round?", "Shia Kensy", 23);
+        var video4 = new Video("Is the earth really round?", "Shia Kensy", 23);
         video4.AddComment(new Comment("Daryll", "Seems pretty round to me!"));
         video4.AddComment(new Comment("Lilly", "Super interesting!"));
         video4.AddComment(new Comment("Carl", "I'd love hear more about this in future videos."));
-        videos.Add(video4);
+        
+        var videos = new List<Video> { video1, video2, video3, video4 };
 
         foreach (var video in videos)
         {
             video.Display();
+            Console.WriteLine();
         }
     }
 }
